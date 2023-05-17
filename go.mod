f420472ba232fba075a640192a7074385f10c850
ahmed <axmedarna3@gmail.com>
2023-05-17 05:52:36 +0300

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/4arnab
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	doubled := transformNumbers(&numbers, double)
+
+	fmt.Println(doubled)
+}
+
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+	for _, value := range *numbers {
+		dNumbers = append(dNumbers, transform(value))
+	}
+
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 2
+}
