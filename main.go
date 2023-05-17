256858f39ad88686b3001e886c52fbf4ad30720a
ahmed <axmedarna3@gmail.com>
2023-05-17 06:07:11 +0300

    functions that returns anohter funcs

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import "fmt"
 type transformFn func(int) int
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6}
+	numbers := []int{2, 3, 4, 5, 6}
 	doubled := transformNumbers(&numbers, double)
 
+	resultTwo := transformNumbers(&numbers, getTransformerFn(&numbers))
+
 	fmt.Println(doubled)
+	fmt.Println(resultTwo)
+}
+
+// function that returns another functions
+func getTransformerFn(numbers *[]int) func(int) int {
+
+	// (dereference before using the numbers slice)
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
